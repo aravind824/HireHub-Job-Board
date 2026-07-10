dd8446a5ba97a8f8bd880bbf77a4d2d42a54f322
Aravind A <aravindarumugam46@gmail.com>
2026-07-10 21:39:23 +0530

    Complete Day 8 - Job CURD Module

--- server/controllers/jobcontroller.js
+++ server/controllers/jobcontroller.js
@@ -20,10 +20,121 @@ exports.createJob = async (req, res) => {
   }
 };
 
-exports.getAllJobs = async (req, res) => {};
 
-exports.getJobById = async (req, res) => {};
 
-exports.updateJob = async (req, res) => {};
+exports.getAllJobs = async (req, res) => {
+  try {
+    const jobs = await Job.find().populate("postedBy", "name email");
 
-exports.deleteJob = async (req, res) => {};
+    res.status(200).json({
+      success: true,
+      count: jobs.length,
+      jobs,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+exports.getJobById = async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id).populate(
+      "postedBy",
+      "name email"
+    );
+
+    if (!job) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      job,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+exports.updateJob = async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id);
+
+    if (!job) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    if (job.postedBy.toString() !== req.user.id) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized",
+      });
+    }
+
+    const updatedJob = await Job.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Job Updated Successfully",
+      job: updatedJob,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+
+exports.deleteJob = async (req, res) => {
+  try {
+    const job = await Job.findById(req.params.id);
+
+    if (!job) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found",
+      });
+    }
+
+    if (job.postedBy.toString() !== req.user.id) {
+      return res.status(403).json({
+        success: false,
+        message: "Not authorized",
+      });
+    }
+
+    await job.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: "Job Deleted Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
